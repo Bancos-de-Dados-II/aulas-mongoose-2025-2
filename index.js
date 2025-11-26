6f48d358e459d0597aed2182b259cafd24f37d20
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-26 16:51:57 -0300

    Conectando ao mongo

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 const {Schema} = mongoose;
 
+conectar();
+async function conectar(){
+    await mongoose
+        .connect('mongodb://localhost:27017/aula');
+}
+
 const anotacaoSchema = new Schema({
     id: {
         type: UUID,
@@ -17,14 +23,17 @@ const anotacaoSchema = new Schema({
         enum: ['pessoal', 'profissional', 'outros']
     },
     localizacao: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true
-    },
-    coordinates: {
-      type: [Number],
-      required: true
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
     }
-  }
-})
+});
+
+const Anotacoes = mongoose
+    .model('Anotacoes', anotacaoSchema);
